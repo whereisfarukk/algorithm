4f09922b6f8131f98315b16462d64491929e0a67
omar faruk <69864169+OOMAR-FARUKK@users.noreply.github.com>
2021-12-10 23:57:15 +0600

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -19,14 +19,14 @@ void solve() {
             v.push_back(x);
         }
         for (int i = 0; i < k; i++) {
-            while (!dq.empty() && dq.back() < v[i]) {
+            while (!dq.empty() && dq.back() < v[i]) {   /*Delete element from the deque until v[i] greater than dq back element.Thats means deque always maintail decreasing oder*/
                 dq.pop_back();
             }
             dq.push_back(v[i]);
         }
         ans.push_back(dq.front());
         for (int i = k; i < n; i++) {
-            if (dq.front() == v[i - k]) {
+            if (dq.front() == v[i - k]) { // if deque first element is no more in the window than delet that item//
                 dq.pop_front();
             }
             while (!dq.empty() && dq.back() < v[i]) {
