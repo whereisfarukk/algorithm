aa6c6de60c14aaf7d0f0788601ccce38c2df95f5
omar faruk <69864169+whereisfarukk@users.noreply.github.com>
2022-05-18 20:24:52 +0600

    separate words from an array

---
+++ WordsFromArray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve() {
+       string str ,S;
+       getline(cin, str);
+       istringstream SS(str);
+       while(SS>>S){
+              cout<<S<<endl;
+       }
+}
+
+int main() {
+
+       solve();
+
+}
